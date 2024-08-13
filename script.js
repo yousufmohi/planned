c1ad8dedb12665096344920abfd9963b30679867
yousufmohi <yousufmohiuddin0529@gmail.com>
2024-08-12 20:12:31 -0400

    Starting Form

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-// NOT STARTED YET
+ const email = document.getElementById("email");
+ const name = document.getElementById("name");
+ const password = document.getElementById("password");
+ const btn = document.getElementById("submit-btn");
+
+ btn.addEventListener("click", function(e) {
+    e.preventDefault();
+    console.log(email.value);
+    console.log(name.value);
+    console.log(password.value);
+ });
+
