953e14006064f85e52541791d06bf1ed7965f9e4
Yousufmohi <yousufmohiuddin0529@gmail.com>
2024-08-15 23:04:24 -0400

    Set up Auth

--- src/script.js
+++ src/script.js
@@ -1,17 +1,20 @@
 // USING version 9.6.10 as newer versions requires using the SDK (i.e installing with npm)
 import { initializeApp, } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
 import {
-  getFirestore,
-  collection,
-  getDocs,
-  addDoc,
+   getFirestore,
+   collection,
+   getDocs,
+   addDoc
 } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js";
 
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
+
 const firebaseConfig = {
-  "[REDACTED]"
+   "[REDACTED]"
 };
 
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 const db = getFirestore();
 // collection has documents which contains fields like "names"
 const ref = collection(db, 'users');
@@ -20,43 +23,40 @@ const ref = collection(db, 'users');
 getDocs(ref).then((snapshot) => {
    let users = []
    snapshot.docs.forEach((doc) => {
-      users.push({...doc.data(),id: doc.id})
+      users.push({ ...doc.data(), id: doc.id })
    })
    console.log(users);
 })
-.catch(err => {
-   console.log(err.message)
-}) 
+   .catch(err => {
+      console.log(err.message)
+   })
 
- const email = document.getElementById("email");
- const name = document.getElementById("name");
- const password = document.getElementById("password");
- const form = document.getElementById("reg-form");
+// 
+const email = document.getElementById("email");
+const name = document.getElementById("name");
+const password = document.getElementById("password");
+const form = document.getElementById("reg-form");
 
- document.getElementById( "reg-form" ).addEventListener("invalid", function( event ) {
-     event.preventDefault();
-     console.log("invalid");
- }, true );
+document.getElementById("reg-form").addEventListener("invalid", function (event) {
+   event.preventDefault();
+   console.log("invalid");
+}, true);
 
- function submitHandler (e) {
-  // prevents page from auto refreshing, allowing for data to save
+
+form.addEventListener("submit", (e) => {
+   // prevents page from auto refreshing, allowing for data to save
    e.preventDefault();
    console.log(email.value);
    console.log(name.value);
    console.log(password.value);
-  //  storing name,email,password in an object
-   var docData = {
-     name: name.value || null,
-     email: email.value || null,
-     password: password.value || null,
-   }
-  // using addDoc to add the document(data) to table
-   addDoc(ref,docData).then(() => {
-     console.log(docData);
-   })
-   .catch(err => {
-     console.log(err.message);
-   }) 
-}
 
- form.addEventListener("submit", submitHandler);
+   createUserWithEmailAndPassword(auth,email.value,password.value).then(cred => {
+      var add = {
+         uid: cred.user.accessToken,
+         name:name.value
+      }
+      addDoc(ref,add);
+   }).catch(err => {
+      console.log(err);
+   });
+});
